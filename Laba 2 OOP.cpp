10d3d1cff7788ba2b2d0223a432630f4cc440303
NazarDorozhynskyi <79474883+NazarDorozhynskyi@users.noreply.github.com>
2021-03-08 21:16:19 +0200

    Update Laba 2 OOP.cpp

---
+++ Laba 2 OOP.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Cone {
+private:
+    int *radius, *height;
+
+public:
+    Cone() {
+        cout << "Викликаємо конструктор..." << endl;
+        radius = new int;
+        height = new int;
+        *radius = *height = 0;
+    }
+
+    Cone(int Radius, int Height) {
+        cout << "Викликаємо конструктор..." << endl;
+        radius = new int;
+        height = new int;
+        *radius = Radius;
+        *height = Height;
+    }
+
+    Cone(const Cone &obj) {
+        cout << "Конструктор копіювання..." << endl;
+        this -> radius = obj.radius;
+        this -> height = obj.height;
+    }
+
+    ~Cone() {
+        cout << "Викликаємо деструктор..." << endl;
+        delete radius;
+        delete height;
+    }
+
+    double GetRadius() {
+        return *radius;
+    }
+
+    double GetHeight() {
+        return *height;
+    }
+
+    void SetRadius(int Radius) {
+        while (Radius <= 0) {
+            cout << "Некоректно значення радіуса! Введіть ще раз: ";
+            cin >> Radius;
+        }
+        *radius = Radius;
+    }
+
+    void SetHeight(int Height) {
+        while (Height <= 0) {
+            cout << "Некоректно значення висоти! Введіть ще раз: ";
+            cin >> Height;
+        }
+        *height = Height;
+    }
+
+    void Print() {
+        cout << "Радіус: " << *radius << "\tВисота: " << *height << "\n\n";
+    }
+
+    double Volume() {
+        return (M_PI * *radius * *radius * *height) / 3;
+    }
+
+    double Area() {
+        double l = sqrt(*height * *height + *radius * *radius); // твірна конуса
+        return M_PI * *radius * (*radius + l); // повна площа поверхні конуса
+    }
+};
+
+int main() {
+    system("chcp 1251");
+
+    Cone obj1(4, 5);
+    cout << "Радіус: " << obj1.GetRadius() << "\tВисота: " << obj1.GetHeight() << endl;
+    obj1.SetRadius(-1);
+    obj1.SetHeight(0);
+    obj1.Print();
+    cout << "Об'єм конуса: " << obj1.Volume() << endl;
+    cout << "Площа повної поверхні конуса: " << obj1.Area() << "\n\n";
+
+    Cone obj2(obj1);
+    obj2.Print();
+
+    Cone obj3;
+    cout << "Радіус: " << obj3.GetRadius() << "\tВисота: " << obj3.GetHeight() << endl;
+    cout << "\nВикликаємо сеттери:\n";
+    obj3.SetRadius(1);
+    obj3.SetHeight(2);
+    obj3.Print();
+    cout << "Об'єм конуса: " << obj3.Volume() << endl;
+    cout << "Площа повної поверхні конуса: " << obj3.Area() << endl;
+
+    getchar();getchar();
+    return 0;
+}
